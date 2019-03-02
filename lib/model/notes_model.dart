cf6c979bb1ab0b25c7d0c6ab894474a271445a64
shareven <1060776877@qq.com>
2019-03-02 21:40:16 +0800

    修复utc时间显示问题

--- lib/model/notes_model.dart
+++ lib/model/notes_model.dart
@@ -11,8 +11,10 @@ class NotesModel implements Comparable<NotesModel>{
   NotesModel.fromJson(json)
   :id=json['id'],
   content=json['content'],
-  // updatedAt=DateTime.parse(json['updatedAt']);
-  updatedAt=DateFormat("yyyy-MM-dd hh:mm:ss").format(DateTime.parse(json['updatedAt']));
+  // updatedAt=DateTime.now().toUtc().toString();
+  // updatedAt=DateFormat("yyyy-MM-dd HH:mm:ss").format(DateTime.parse("2002-01-27T14:00:00-0500"));
+  updatedAt=DateFormat("yyyy-MM-dd HH:mm:ss").format(DateTime.parse("${json['updatedAt'].substring(0,19)}-0801"));
+  // updatedAt=DateFormat("yyyy-MM-dd HH:mm:ss").format(DateTime.parse("2019-03-01T14:48:28Z"));
 
    @override
   int compareTo(NotesModel other) => updatedAt.compareTo(other.updatedAt);
